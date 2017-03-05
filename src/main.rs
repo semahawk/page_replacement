5c37dbf9d53034a85a7e63a8618d404a0fbd1d4d
Szymon Urbaś <szymon.urbas@aol.com>
2017-03-05 22:21:36 +0100

    Add the definitions for the page size (and divide to get the number of them)

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,16 @@ mod util;
 
 const PHYS_RAM_SIZE: u64 = MiB!(128);
 const VIRT_RAM_SIZE: u64 = GiB!(4);
+const PAGE_SIZE: u64 = KiB!(4);
+const PHYS_PAGE_NUM: u64 = PHYS_RAM_SIZE / PAGE_SIZE;
+const VIRT_PAGE_NUM: u64 = VIRT_RAM_SIZE / PAGE_SIZE;
 
 fn main() {
   println!("Physical address space size: {}", util::to_nice_unit(PHYS_RAM_SIZE));
-  println!("Virtual address space size:  {}", util::to_nice_unit(VIRT_RAM_SIZE));
+  println!("Virtual address space size: {}", util::to_nice_unit(VIRT_RAM_SIZE));
+  println!("Page size: {}", util::to_nice_unit(PAGE_SIZE));
+  println!("Physical page num: {}", PHYS_PAGE_NUM);
+  println!("Virtual page num: {}", VIRT_PAGE_NUM);
 }
 
 //
