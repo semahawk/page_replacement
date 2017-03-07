0eb5b7e9f6c274973ceb7d4e0b1a934056d463d1
Szymon Urbaś <szymon.urbas@aol.com>
2017-03-07 21:50:15 +0100

    Print some summary at the end

--- src/memory.rs
+++ src/memory.rs
@@ -16,6 +16,8 @@ pub struct Memory {
   page_size: usize,
   vm_page_directory: HashMap<u32, Page>,
   phys_mem_size_left: usize,
+  page_fault_num: usize,
+  page_access_num: usize,
 }
 
 pub fn new(phys_mem_size: usize, virt_mem_size: usize, page_size: usize) -> Memory {
@@ -23,15 +25,19 @@ pub fn new(phys_mem_size: usize, virt_mem_size: usize, page_size: usize) -> Memo
     page_size: page_size,
     vm_page_directory: HashMap::with_capacity(virt_mem_size / page_size),
     phys_mem_size_left: phys_mem_size,
+    page_fault_num: 0,
+    page_access_num: 0,
   }
 }
 
 impl Memory {
   pub fn access_page(&mut self, page: u32) {
+    self.page_access_num += 1;
     if self.vm_page_directory.contains_key(&page) {
       println!("accessed page 0x{:x}", page);
     } else {
       println!("page fault on page: 0x{:x}", page);
+      self.page_fault_num += 1;
       if self.phys_mem_size_left >= self.page_size {
         // 'allocate' a page frame
         self.phys_mem_size_left -= self.page_size;
@@ -42,6 +48,13 @@ impl Memory {
         println!("no physical memory left! swapping a page to disk");
       }
     }
+  }
+}
+
+impl Drop for Memory {
+  fn drop(&mut self) {
+    println!("summary: on {} page accesses there was {} page faults",
+      self.page_access_num, self.page_fault_num);
   }
 }
 
